8f410efbd652939362e40562eccff785e11a0102
Kevin Brenneman <kalvin66rocks@gmail.com>
2015-11-08 15:35:41 -0500

    just have to sort on lab 9

--- l9p1_kjb5568.asm
+++ l9p1_kjb5568.asm
@@ -3,10 +3,11 @@ stack_beg:
         .word   0 : 40
 stack_end:
 unsorted_array: .float 0.0:20
-sorted_array: 	.float 0.0:20
+#sorted_array: 	.float 0.0:20
 total:		.float 0.0
 number: 	.float 5.0
 
+space:		.asciiz " "
 prompt: .asciiz "Enter the number of floats to enter"
 
 
@@ -18,6 +19,7 @@ prompt: .asciiz "Enter the number of floats to enter"
 	li $v0, 5
 	syscall
 	move $t1, $v0
+	move $s0, $t1
 	mtc1 $t1, $f8
 	cvt.s.w $f5, $f8
 Loop:
@@ -29,20 +31,46 @@ Loop:
 	add.s $f10, $f10, $f0
 	bnez $t1, Loop
 ###################################################################
-	mov.s $f12, $f10
-	li $v0, 2
-	syscall
+	#mov.s $f12, $f10
+	#li $v0, 2
+	#syscall
 ######################################
 ####Average
 	div.s $f6, $f10, $f5
-	addi $a0,$0,0xA		#print a new line and you lose
+	addi $a0,$0,0xA		#print a new line
 	li $v0, 11
 	syscall
 	mov.s $f12, $f6
 	li $v0, 2
 	syscall
+	addi $a0,$0,0xA		#print a new line
+	li $v0, 11
+	syscall
 #######################################
+
+
+
+######################################################################
+#print sorted array
+#######################################################################
+	la $t2, unsorted_array
+PrintLoop:
+	l.s $f12, 0($t2)
+	li $v0, 2
+	syscall
+	addiu $s0, $s0, -1
+	addiu $t2, $t2, 4
+	la $a0, space
+	li $v0, 4
+	syscall
+	bnez $s0, PrintLoop
 	
+	
+	
+	
+#######################################################################
+#exit the program
+#######################################################################
 	li $v0, 10
 	syscall
 
